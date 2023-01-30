cca36077b1fbe79c35dcce5bc22b3bb2644a1a63
dakuto <49013183+dakuto@users.noreply.github.com>
2023-01-30 21:01:16 +0900

    課題3-2の修正_20230130
    アドバイスを元に修正しました。
    とりあえず、エラーは出なくなりましたが確認お願いします。

--- 3-2/index.php
+++ 3-2/index.php
@@ -1,6 +1,7 @@
 <?php
 $fruits = ["りんご" => 50, "みかん" => 50, "もも" => 300];
 $fruitAmount = [6, 3, 10];
+$i = 0;
 
 function getPrice($unitPrice, $quantity)
 {
@@ -9,7 +10,7 @@ function getPrice($unitPrice, $quantity)
 }
 
 foreach ($fruits as $key => $value) {
-    echo $key . "は" . getPrice($value, $fruitAmount[$i]) . "円です。"."<br>";
+    echo $key . "は" . getPrice($value, $fruitAmount[$i]) . "円です。" . "<br>";
     $i++;
     // var_dump($fruitAmount);
 }
