51cdbc4cea66069c29675123ef0cfa1bc2e0de27
dakuto <49013183+dakuto@users.noreply.github.com>
2023-02-06 23:19:12 +0900

    3章練習問題２（20230206）
    お疲れ様です。
    アドバイスいただいたのをヒントにしながら制作してみました。
    確認よろしくお願いします。

--- 3-3/result.php
+++ 3-3/result.php
@@ -1,6 +1,17 @@
 <?php
 $number = $_POST['number'];
 var_dump($_POST);
+$num_len = strlen($number);
+echo ('<br>');
+var_dump($num_len);
+echo ('<br>');
+$num_rand = mt_rand(0, $num_len);
+var_dump($num_rand);
+echo ('<br>');
+$num_subs = substr($number, $num_rand, 1);
+var_dump($num_subs);
+echo ('<br>');
+
 ?>
 
 <!DOCTYPE html>
@@ -14,8 +25,23 @@ var_dump($_POST);
 </head>
 
 <body>
-    <p><?php echo date("Y/m/d") ?>の運勢は</p>
-    <p>選ばれた数字は</p>
+    <p><?php echo date("Y/m/d"); ?>の運勢は</p>
+    <p>選ばれた数字は<?php echo substr($number, $num_rand, 1); ?></p>
+    <p>
+        <?php
+        if ($num_subs == 0) {
+            echo "凶";
+        } elseif ($num_subs <= 3) {
+            echo "小吉";
+        } elseif ($num_subs <= 6) {
+            echo "中吉";
+        } elseif ($num_subs <= 8) {
+            echo "吉";
+        } elseif ($num_subs == 9) {
+            echo "大吉";
+        }
+        ?>
+    </p>
 
 </body>
 
