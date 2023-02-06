9c19581d50c5d20246ad4b944b74644b31481109
dakuto <49013183+dakuto@users.noreply.github.com>
2023-02-06 11:47:57 +0900

    3章練習問題２
    チャットで質問させていただきました。
    今、とりあえずできている分までをお手数ですがご確認いただき、アドバイスをいただけたら幸いです。

---
+++ 3-3/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>3章練習問題2</title>
+</head>
+
+<body>
+    <p>0～9の番号を使って好きな数字の羅列をにゅうりょくしてください。</p>
+    <form action="result.php" method="post">
+        <input type="text" name="number">
+        <input type="submit" value="占う">
+    </form>
+</body>
+
+</html>
---
+++ 3-3/result.php
@@ -0,0 +1,22 @@
+<?php
+$number = $_POST['number'];
+var_dump($_POST);
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>３章練習問題2｜結果</title>
+</head>
+
+<body>
+    <p><?php echo date("Y/m/d") ?>の運勢は</p>
+    <p>選ばれた数字は</p>
+
+</body>
+
+</html>
