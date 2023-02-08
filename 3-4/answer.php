2d315c205f09fb27dfb86a5dd240af12d7b1402f
dakuto <49013183+dakuto@users.noreply.github.com>
2023-02-09 00:09:46 +0900

    3章チェックテスト20230208
    残りのページも作成。
    一番最初のページで取得した名前を回答用のページまで引き継いでいくには？

---
+++ 3-4/answer.php
@@ -0,0 +1,64 @@
+<?php
+$question1 = $_POST['question1'];
+$answer1 = $_POST['answer1'];
+
+$question2 = $_POST['question2'];
+$answer2 = $_POST['answer2'];
+
+$question3 = $_POST['question3'];
+$answer3 = $_POST['answer3'];
+
+var_dump($question1, $question2, $question3);
+var_dump($answer1, $answer2, $answer3);
+
+if ($question1 == $answer1) {
+    $result1 = "正解！";
+} else {
+    $result1 = "残念・・・";
+}
+
+if ($question2 == $answer2) {
+    $result2 = "正解！";
+} else {
+    $result2 = "残念・・・";
+}
+
+if ($question3 == $answer3) {
+    $result3 = "正解！";
+} else {
+    $result3 = "残念・・・";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2章チェックテスト｜回答</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+
+    <h2>①の答え</h2>
+    <p>
+        <?php echo $result1; ?>
+    </p>
+
+    <h2>②の答え</h2>
+    <p>
+        <?php echo $result2; ?>
+    </p>
+
+    <h2>③の答え</h2>
+    <p>
+        <?php echo $result3; ?>
+    </p>
+
+</body>
+
+</html>
---
+++ 3-4/question.php
@@ -0,0 +1,56 @@
+<?php
+$user_name = $_POST['user_name'];
+
+$question1 = ["80", "22", "20", "21"];
+$question2 = ["PHP", "Python", "JAVA", "HTML"];
+$question3 = ["join", "select", "insert", "update"];
+
+$answer1 = $question1[0];
+$answer2 = $question2[3];
+$answer3 = $question3[1];
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2章チェックテスト｜質問</title>
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+    <div class="container question_container">
+        <p>お疲れ様です<?php echo $user_name; ?>さん</p>
+
+        <form action="answer.php" method="post">
+            <h2>①ネットワークのポート番号は何番？</h2>
+            <?php foreach ($question1 as $value) { ?>
+                <input type="radio" name="question1" value="<?php echo $value; ?>"><?php echo $value; ?>
+            <?php } ?>
+            <input type="hidden" name="answer1" value="<?php echo $answer1; ?>">
+
+            <h2>②webページを作成するための言語は？</h2>
+            <?php foreach ($question2 as $value) { ?>
+                <input type="radio" name="question2" value="<?php echo $value; ?>"><?php echo $value; ?>
+            <?php } ?>
+            <input type="hidden" name="answer2" value="<?php echo $answer2; ?>">
+
+            <h2>③MySQLで情報を取得するためのコマンドは？</h2>
+            <?php foreach ($question3 as $value) { ?>
+                <input type="radio" name="question3" value="<?php echo $value; ?>"><?php echo $value; ?>
+            <?php } ?>
+            <input type="hidden" name="answer3" value="<?php echo $answer3; ?>">
+            <br>
+            <div class="question_button">
+                <input type="submit" value="回答する">
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
