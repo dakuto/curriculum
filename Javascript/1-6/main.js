0ae100379e1f6367e423e5e79a8ae1ad2237c329
dakuto <49013183+dakuto@users.noreply.github.com>
2023-02-13 23:14:06 +0900

    課題チャレンジ
    チャレンジ問１と問２を制作

---
+++ Javascript/1-6/main.js
@@ -0,0 +1,16 @@
+// チャレンジ問１
+let scores = [10,15,20,25];
+for(let i = 0; i < scores.length; i++){
+    if(i % 2 === 0){
+        console.log(`${scores[i]}は偶数です`);
+    }
+}
+
+// チャレンジ問２
+let car = {
+    gass:'20.5',
+    num:1234,
+};
+console.log(car);
+console.log(`ガソリンは${car.gass}です`);
+console.log(`ナンバーは${car.num}です`);
