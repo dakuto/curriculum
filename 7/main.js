fe1853118b2049db091159bf588206f19db0fb08
dakuto <49013183+dakuto@users.noreply.github.com>
2023-02-15 22:30:23 +0900

    7章チェックテストの続き20230215
    アドバイスを貰って少しだけ進められたが、まだうまく理解できていないところがあり、行き詰っています。

--- 7/main.js
+++ 7/main.js
@@ -1,6 +1,10 @@
 let numbers = [2, 5, 12, 13, 15, 18, 22];
-function isEven() {
-  let num = numbers[0] % 2 === 0;
-  console.log(num + "は偶数です");
+for (let i = 0; i < numbers.length; i++) {
+  // console.log(numbers[i]);
+
+  isEven();
 }
-isEven();
+// function isEven() {
+//   console.log(num + "は偶数です");
+// }
+// isEven();
