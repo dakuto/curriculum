ffaa8ec708cdc9a2ec68e5af6952addc76669ec5
dakuto <49013183+dakuto@users.noreply.github.com>
2023-02-15 22:51:45 +0900

    7章チェックテスト

--- 7/main.js
+++ 7/main.js
@@ -1,10 +1,12 @@
 let numbers = [2, 5, 12, 13, 15, 18, 22];
 for (let i = 0; i < numbers.length; i++) {
   // console.log(numbers[i]);
-
-  isEven();
+  let $numbers = numbers[i];
+  isEven($numbers[i]);
 }
-// function isEven() {
-//   console.log(num + "は偶数です");
-// }
+function isEven(num) {
+  if(numbers[i] % 2 === 0){
+    console.log(num + "は偶数です");
+  }
+}
 // isEven();
