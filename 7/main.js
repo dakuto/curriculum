1648743d4d3cbda3ffc4e8eba621b25f579bc42e
dakuto <49013183+dakuto@users.noreply.github.com>
2023-02-16 22:37:41 +0900

    7章、問１修正と問２の作成
    アドバイスありがとうございます。
    問１の修正と問２の作成をしました。
    確認をよろしくお願いします。

--- 7/main.js
+++ 7/main.js
@@ -1,12 +1,24 @@
+// 問1
 let numbers = [2, 5, 12, 13, 15, 18, 22];
 for (let i = 0; i < numbers.length; i++) {
   // console.log(numbers[i]);
-  let $numbers = numbers[i];
-  isEven($numbers[i]);
+  isEven(numbers[i]);
 }
 function isEven(num) {
-  if(numbers[i] % 2 === 0){
+  if (num % 2 === 0) {
     console.log(num + "は偶数です");
   }
 }
-// isEven();
+
+// 問２
+class car {
+  constructor(gas, num) {
+    this.gas = gas;
+    this.num = num;
+  }
+  getNumGas() {
+    console.log(`ガソリンは${this.gas}です。ナンバーは${this.num}です`);
+  }
+}
+let toyota = new car("ハイオク", 1234);
+toyota.getNumGas();
