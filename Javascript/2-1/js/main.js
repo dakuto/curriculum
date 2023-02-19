a01954a5707e65aca3cd603f0f423491ab016741
dakuto <49013183+dakuto@users.noreply.github.com>
2023-02-19 20:36:00 +0900

    ８章、8-3の作成

---
+++ Javascript/2-1/js/main.js
@@ -0,0 +1,18 @@
+// function myfunc() {
+//   let change = document.getElementById("target");
+//   change.textContent = "こんにちは！";
+// }
+
+// let elems = document.getElementsByTagName("h1");
+
+// console.log(elems[0]);
+// console.log(elems[1]);
+
+// let boxs = document.getElementsByClassName("box");
+// for (let i = 0; i < boxs.length; i++) {
+//   console.log(boxs[i]);
+// }
+
+let changeColor = document.getElementById("box");
+changeColor.style.backgroundColor = "#ff0000";
+console.log(changeColor);
